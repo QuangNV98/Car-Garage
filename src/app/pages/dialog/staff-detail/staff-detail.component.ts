0a342798209f4d262fbfeef5ccc7663f136ee7df
quangnv <quangoc98@gmail.com>
2020-10-28 22:34:14 +0700

    test

--- src/app/pages/dialog/staff-detail/staff-detail.component.ts
+++ src/app/pages/dialog/staff-detail/staff-detail.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staff-detail.component.css']
 })
 export class StaffDetailComponent implements OnInit {
-
+//alo
   constructor() { }
 
   ngOnInit(): void {
